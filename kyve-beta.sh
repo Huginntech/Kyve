8c0f6e3d10fc54e20e061fc1ac824708ea1b7fcd
Errorist <71728280+Errorist79@users.noreply.github.com>
2022-04-06 19:58:10 +0300

    Update kyve-beta.sh

--- kyve-beta.sh
+++ kyve-beta.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
-dependient () {
+echo -e ''
+curl -s https://api.testnet.run/logo.sh | bash && sleep 3
+echo -e ''
+
+dependiences () {
+    echo -e '\e[0;33mİnstalling Dependiences\e[0m'
+    echo -e ''
     sudo apt update
+    sudo apt install jq -y
     sudo apt install zip -y
     sudo apt install screen -y
     sudo apt install make clang pkg-config libssl-dev build-essential git jq ncdu nload -y < "/dev/null"
+}
+
+variables (){
+    echo export CHAIN_ID=kyve-beta >> $HOME/.profile
+    echo export denom=tkyve >> $HOME/.profile
+    source $HOME/.profile
+}
+
+
+
+binaries () {
+    echo -e ''
+    echo -e '\e[0;33mDownload Binaries\e[0m' && sleep 1
     echo -e ''
     echo -e "\033[1;34m"
     if [ ! $node_name ]; then
@@ -12,7 +32,19 @@ dependient () {
     fi
     . $HOME/.bash_profile
     echo -e "\033[0m"
+    mkdir kyvebinary && cd kyvebinary
+    wget -q https://free.files.cnow.at/index.php/s/Y828KHtSgbg5PYe/download/chaind
+    sudo mv chaind kyved
+    sudo chmod +x kyved
+    sudo mv kyved /usr/bin/
+    kyved config chain-id kyve-beta
+    wget -q -O ~/.kyve/config/genesis.json https://raw.githubusercontent.com/Errorist79/Kyve/main/genesis.json
+    echo -e "\033[1;34m"
+}
+
+golang () {
     echo -e "\e[0;33mGo...\e[0m" && sleep 2
+    echo -e ""
 	wget -O go1.17.1.linux-amd64.tar.gz https://golang.org/dl/go1.17.linux-amd64.tar.gz
 	rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.1.linux-amd64.tar.gz && rm go1.17.1.linux-amd64.tar.gz
 	echo 'export GOROOT=/usr/local/go' >> $HOME/.bash_profile
@@ -23,28 +55,10 @@ dependient () {
 	go version
 }
 
-variables (){
-    echo export CHAIN_ID=kyve-beta >> $HOME/.profile
-    echo export denom=tkyve >> $HOME/.profile
-    source $HOME/.profile
-}
-
-select_dist () {
-    source $HOME/.profile && sleep 2
-    VAR=$(cat $HOME/distro.txt)
-    source $HOME/.profile
-    if [ $VAR = "Darwin" ]; then
-     echo -e "installing cosmovisor for MacOS.." && sleep 3
-     wget -q -O "$HOME"/cosmovisor https://nc.breithecker.de/index.php/s/7o7q26WXciQbFnR/download/cosmovisor_mac
-else
-     echo -e "installing cosmovisor for Linux.." && sleep 3
-     wget -q -O "$HOME"/cosmovisor https://nc.breithecker.de/index.php/s/6PccY4zznJN4H4L/download/cosmovisor_linux
-fi
-}
-
 cosmovisor () {
     echo -e ''
-    echo -e "Cosmovisor..." && sleep 2
+    echo -e "\e[33mCosmovisor...\e[0m" && sleep 2
+    echo -e ""
     git clone https://github.com/cosmos/cosmos-sdk
     cd cosmos-sdk
     git checkout v0.44.3
@@ -53,25 +67,6 @@ cosmovisor () {
     cp cosmovisor/cosmovisor $GOPATH/bin/cosmovisor
     cd $HOME
 }
-
-
-create_bin () {
-    wget -q -O /usr/bin/kyved https://free.files.cnow.at/index.php/s/Y828KHtSgbg5PYe/download/chaind && chmod +x /usr/bin/kyved
-    mkdir -p ~/.kyve/cosmovisor/genesis/bin/
-    echo "{}" > ~/.kyve/cosmovisor/genesis/upgrade-info.json
-    cp /usr/bin/kyved ~/.kyve/cosmovisor/genesis/bin/
-    export DAEMON_HOME="$HOME/.kyve" >> $HOME/.profile
-    export DAEMON_NAME="kyved" >> $HOME/.profile
-    # optional on whether auto-download should be enabled
-    export DAEMON_ALLOW_DOWNLOAD_BINARIES="true" >> $HOME/.profile
-    source ~/.profile
-    sleep 2
-    kyved config chain-id kyve-beta
-    wget -q -O ~/.kyve/config/genesis.json https://raw.githubusercontent.com/Errorist79/Kyve/main/genesis.json
-    wget https://raw.githubusercontent.com/Errorist79/seeds/main/seeds.txt -O $HOME/.kyve/config/seeds.txt
-    sed -i.bak 's/seeds = \"\"/seeds = \"'$(cat $HOME/.kyve/config/seeds.txt)'\"/g' $HOME/.kyve/config/config.toml
-}
-
 
 key_gen () {
     echo -e "\033[1;34m"
@@ -98,7 +93,24 @@ fi
     echo -e '\n\e[44mHere is the your wallet address, save it!:' $KYVE_WALLET '\e[0m\n'
 }
 
-services () {
+visor_bin () {
+    wget -q -O /usr/bin/kyved https://free.files.cnow.at/index.php/s/Y828KHtSgbg5PYe/download/chaind && chmod +x /usr/bin/kyved
+    mkdir -p ~/.kyve/cosmovisor/genesis/bin/
+    echo "{}" > ~/.kyve/cosmovisor/genesis/upgrade-info.json
+    cp /usr/bin/kyved ~/.kyve/cosmovisor/genesis/bin/
+    export DAEMON_HOME="$HOME/.kyve" >> $HOME/.profile
+    export DAEMON_NAME="kyved" >> $HOME/.profile
+    # optional on whether auto-download should be enabled
+    export DAEMON_ALLOW_DOWNLOAD_BINARIES="true" >> $HOME/.profile
+    source ~/.profile
+    sleep 2
+}
+
+config_service () {
+    wget https://raw.githubusercontent.com/Errorist79/seeds/main/seeds.txt -O $HOME/.kyve/config/seeds.txt
+    sed -i.bak 's/seeds = \"\"/seeds = \"'$(cat $HOME/.kyve/config/seeds.txt)'\"/g' $HOME/.kyve/config/config.toml
+    echo -e 'Creating system service...'
+    sleep 2
 sudo tee /etc/systemd/system/kyved.service > /dev/null <<EOF  
 [Unit]
 Description=Kyve Daemon
@@ -121,6 +133,7 @@ EOF
 sudo -S systemctl daemon-reload
 sudo -S systemctl enable kyved
 sudo -S systemctl start kyved
+sleep 2
 sed -i 's/#Storage=auto/Storage=persistent/g' /etc/systemd/journald.conf
 sudo systemctl restart systemd-journald
 }
@@ -150,27 +163,25 @@ create_validator () {
 done_process () {
     LOG_SEE="journalctl -u kyved.service -f -n 100"
     source $HOME/.profile
-    echo -e '\n\e[41mDone! Now, please wait for your node to sync with the chain. This will take approximately 1h. Use this command to see the logs:' $LOG_SEE '\e[0m\n'
+    echo -e '\n\e[41mDone! Now, please wait for your node to sync with the chain. This will take approximately 15 minutes. Use this command to see the logs:' $LOG_SEE '\e[0m\n'
 }
 
 PS3="What do you want?: "
 select opt in İnstall Update Additional quit; 
 do
 
-case $opt in
-    İnstall)   
+  case $opt in
+    İnstall)
     echo -e '\e[1;32mThe installation process begins...\e[0m'
-    uname >> $HOME/distro.txt
-    export VAR=$(cat $HOME/distro.txt)
-    source $HOME/.profile
     sleep 1
-    #select_dist
-    dependient
+    dependiences
     variables
+    binaries
+    golang
     cosmovisor
     key_gen
-    create_bin
-    services
+    visor_bin
+    config_service
     create_validator
     done_process
     sleep 3
@@ -180,6 +191,7 @@ case $opt in
     echo -e '\e[1;32mThe updating process begins...\e[0m'
     echo -e ''
     echo -e '\e[1;32mSoon...'
+    done_process
     sleep 1
       break
       ;;
